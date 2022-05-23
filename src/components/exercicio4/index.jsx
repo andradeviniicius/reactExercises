93a1ee8c955be6e2595f62ad6f7e8900cd540b89
andradeviniicius <viniciusdandrade01@gmail.com>
2022-05-23 17:43:51 -0300

    challenge4 complete

--- src/components/exercicio4/index.jsx
+++ src/components/exercicio4/index.jsx
@@ -1,5 +1,26 @@
+import { useState } from "react"
+
+
 function Exercicio4() {
-	return <h1 className='title'>Exercicio4</h1>
+	const [counter,setCounter] = useState(0)
+
+	return (
+		<>
+		 <div
+        style={{
+          display: "flex",
+		  flexDirection:'column',
+          alignItems: "center",
+          justifyContent: "start",
+          width: "100%",
+          height: "100%",
+        }}
+      >
+		  <p>You have clicked me {counter} times</p>
+			<button onClick={()=>{setCounter(counter+1)}}>Click me</button>
+	  </div>
+		</>
+	)
 }
 
 export default Exercicio4
