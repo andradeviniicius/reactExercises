8287a89abc19eea67f937ee3cd34b93ff75b99fe
andradeviniicius <viniciusdandrade01@gmail.com>
2022-05-24 12:36:34 -0300

    complete!

--- src/components/exercicio5/index.jsx
+++ src/components/exercicio5/index.jsx
@@ -1,5 +1,32 @@
+import { useState } from "react";
+
 function Exercicio5() {
-	return <h1 className='title'>Exercicio5</h1>
+  const animals = ["dog", "cat", "chicken", "cow", "sheep", "horse"];
+  
+  const animalList = animals.map((a) =>
+  <li>{a}</li>
+);
+
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "start",
+          width: "100%",
+          height: "100%",
+        }}
+      >
+
+		<ul>
+			{animalList}
+		</ul>
+
+	  </div>
+    </>
+  );
 }
 
-export default Exercicio5
+export default Exercicio5;
